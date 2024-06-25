d576fcc86541ed965fe0aec3ec9071ff11aab091
ohigashi-tky <roziura8114@yahoo.co.jp>
2024-06-25 20:25:18 +0900

    Initial Commit

---
+++ side-scrolling-gameUITests/side_scrolling_gameUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  side_scrolling_gameUITestsLaunchTests.swift
+//  side-scrolling-gameUITests
+//
+//  Created by 大東拓也 on 2024/06/25.
+//
+
+import XCTest
+
+final class side_scrolling_gameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
