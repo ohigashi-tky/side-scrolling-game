d06308c4422e6115a60df3cf8f099bc2f7d11149
ohigashi-tky <97085725+ohigashi-tky@users.noreply.github.com>
2024-06-28 00:21:32 +0900

    BGM追加、コインの追加、当たり判定の調整

--- side-scrolling-game/GameScene.swift
+++ side-scrolling-game/GameScene.swift
@@ -7,15 +7,24 @@
 
 import SpriteKit
 import GameplayKit
+import AVFoundation
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
+    private var backgroundMusic: AVAudioPlayer?
     private var player: SKSpriteNode!
     private var ground: SKSpriteNode!
+    private var coins: [SKSpriteNode] = []
     private var scoreLabel: SKLabelNode!
     private var score = 0 {
         didSet {
             scoreLabel.text = "スコア: \(score)"
+        }
+    }
+    private var coinAmountLabel: SKLabelNode!
+    private var coinAmount = 0 {
+        didSet {
+            coinAmountLabel.text = "\(coinAmount) ビットコイン"
         }
     }
     private var gameOverLabel: SKLabelNode!
@@ -29,11 +38,54 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
         physicsWorld.gravity = CGVector(dx: 0, dy: -5) // 重力を調整
+        setupBackgroundMusic()
         setupBackground()
         setupGround()
         setupPlayer()
         setupScore()
+        setupCoinAmount()
         startObstacleGeneration()
+        setupCoins()
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print("Failed to set audio session category.")
+        }
+    }
+    
+    func setupBackgroundMusic() {
+        guard let path = Bundle.main.path(forResource: "bgm1", ofType: "mp3") else {
+            print("BGM file not found. Searched in bundle: \(Bundle.main.bundlePath)")
+            return
+        }
+        
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            backgroundMusic = try AVAudioPlayer(contentsOf: url)
+            backgroundMusic?.numberOfLoops = -1
+            backgroundMusic?.volume = 0.5
+            backgroundMusic?.play()
+            print("BGM should be playing now")
+        } catch {
+            print("Couldn't load BGM: \(error)")
+        }
+    }
+    
+    // BGMの一時停止
+    func pauseBackgroundMusic() {
+        backgroundMusic?.pause()
+    }
+    
+    // BGMの再開
+    func resumeBackgroundMusic() {
+        backgroundMusic?.play()
+    }
+    
+    // BGMの停止
+    func stopBackgroundMusic() {
+        backgroundMusic?.stop()
     }
     
     func setupBackground() {
@@ -59,7 +111,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         player.size = CGSize(width: 60, height: 60) // 必要に応じてサイズを調整
         player.xScale = -1
         player.position = CGPoint(x: frame.minX + 100, y: ground.position.y + ground.size.height / 2 + player.size.height / 2 + 10) // 位置を少し上げる
-        let smallerSize = CGSize(width: player.size.width * 0.8, height: player.size.height * 0.8)
+        let smallerSize = CGSize(width: player.size.width * 0.7, height: player.size.height * 1)
         player.physicsBody = SKPhysicsBody(rectangleOf: smallerSize)
         player.physicsBody?.isDynamic = true
         player.physicsBody?.allowsRotation = false
@@ -77,11 +129,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(scoreLabel)
     }
     
+    func setupCoinAmount() {
+        coinAmountLabel = SKLabelNode(fontNamed: "Arial")
+        coinAmountLabel.text = "0 ビットコイン"
+        coinAmountLabel.position = CGPoint(x: frame.minX + 130, y: frame.maxY - 140)
+        addChild(coinAmountLabel)
+    }
+    
     // 障害物の生成
     func startObstacleGeneration() {
         let createAndWait = SKAction.run { [weak self] in
             self?.spawnObstacle()
-            let waitDuration = Double.random(in: 1.5...3)
+            let waitDuration = Double.random(in: 2...4)
             self?.run(SKAction.wait(forDuration: waitDuration)) { [weak self] in
                 self?.startObstacleGeneration()
             }
@@ -97,7 +156,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         obstacle.size = CGSize(width: 80, height: 80) // 必要に応じてサイズを調整
         obstacle.position = CGPoint(x: frame.maxX + obstacle.size.width / 2, y: ground.position.y + ground.size.height / 2 + obstacle.size.height / 2)
         // 物理ボディのサイズを画像サイズより小さくする
-        let smallerSize = CGSize(width: obstacle.size.width * 0.8, height: obstacle.size.height * 0.8)
+        let smallerSize = CGSize(width: obstacle.size.width * 0.7, height: obstacle.size.height * 1)
         obstacle.physicsBody = SKPhysicsBody(rectangleOf: smallerSize)
         obstacle.physicsBody?.isDynamic = false
         obstacle.physicsBody?.categoryBitMask = PhysicsCategory.obstacle
@@ -106,18 +165,35 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let moveLeft = SKAction.moveBy(x: -frame.width - obstacle.size.width, y: 0, duration: 4.0)
         let remove = SKAction.removeFromParent()
         obstacle.run(SKAction.sequence([moveLeft, remove]))
-        
-//        // 画面外に出た障害物を確実に削除
-//        let cleanup = SKAction.run { [weak self] in
-//            self?.enumerateChildNodes(withName: "obstacle") { node, _ in
-//                if node.position.x < self!.frame.minX - node.frame.width {
-//                    node.removeFromParent()
-//                }
-//            }
-//        }
-//        run(SKAction.sequence([SKAction.wait(forDuration: 5.0), cleanup]))
     }
 
+    func setupCoins() {
+        // コインを定期的に生成するアクション
+        let createCoinAction = SKAction.run { [weak self] in
+            self?.createCoin()
+        }
+        let waitAction = SKAction.wait(forDuration: 1.5) // コインの生成間隔
+        let sequence = SKAction.sequence([createCoinAction, waitAction])
+        let repeatForever = SKAction.repeatForever(sequence)
+        
+        run(repeatForever)
+    }
+    
+    func createCoin() {
+        let coin = SKSpriteNode(imageNamed: "Coin") // コインの画像を使用
+        coin.size = CGSize(width: 30, height: 30) // コインのサイズを調整
+        // 画面の高さの中央40%の範囲内でランダムなY座標を生成
+        let minY = size.height * 0.15 // 画面の下から30%の位置
+        let maxY = size.height * 0.35 // 画面の下から70%の位置
+        let randomY = CGFloat.random(in: minY...maxY)
+        
+        coin.position = CGPoint(x: size.width + coin.size.width/2,
+                                y: randomY)
+        
+        addChild(coin)
+        coins.append(coin)
+    }
+    
     // タッチした時
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         print("touchesBegan")
@@ -125,7 +201,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         if let velocity = player.physicsBody?.velocity {
             print("Player velocity.dy at touch: \(velocity.dy)")
             
-            let tolerance: CGFloat = 0.1 // 許容範囲を設定
+            let tolerance: CGFloat = 5 // 許容範囲を設定
             if abs(velocity.dy) < tolerance { // 速度の絶対値が許容範囲内であればジャンプを許可
                 player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 55)) // ジャンプ力を調整
                 print("Jump applied")
@@ -150,8 +226,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         // ゲームを停止
         self.isPaused = true
         
-        // スコアをリセット
+        // スコア、金額をリセット
         score = 0
+        coinAmount = 0
         
         // ゲームオーバーラベルを表示
         if gameOverLabel == nil {
@@ -189,5 +266,37 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     override func update(_ currentTime: TimeInterval) {
         score += 1
+
+        moveCoins()
+        checkCoinCollisions()
+    }
+    
+    func moveCoins() {
+        for (index, coin) in coins.enumerated().reversed() {
+            coin.position.x -= 4 // コインの移動速度
+            
+            if coin.position.x < -coin.size.width/2 {
+                coin.removeFromParent()
+                coins.remove(at: index)
+            }
+        }
+    }
+    
+    func checkCoinCollisions() {
+        for (index, coin) in coins.enumerated().reversed() {
+            if player.frame.intersects(coin.frame) {
+                collectCoin(coin: coin, at: index)
+            }
+        }
+    }
+    
+    // コインを集めた時
+    func collectCoin(coin: SKSpriteNode, at index: Int) {
+        coin.removeFromParent()
+        coins.remove(at: index)
+        coinAmount += 1
+        coinAmountLabel.text = "\(coinAmount) ビットコイン"
+        
+        // コイン収集時のエフェクトやサウンドを追加することもできます
     }
 }
